67b5fa9266e400f7f381c2f0bc9902157155536f
Nathan Chen <124654225+nathanc7278@users.noreply.github.com>
2025-05-29 21:13:32 -0700

    code

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -57,28 +57,30 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
         lock_acquire(&syscall_lock);
         struct file* new_file = filesys_open((const char *) args[1]);
         if (new_file == NULL) {
-            lock_release(&syscall_lock);
             f->eax = -1;
+        } else {
+            int fd_index = 2;
+            while (thread_current()->fd_table[fd_index] != NULL && fd_index < 128) {
+                fd_index++;
+            }
+            if (fd_index > 127) {
+                f->eax = -1;
+            }
+            else {
+                thread_current()->fd_table[fd_index] = new_file;
+                f->eax = fd_index;
+            }
         }
-        int fd_index = 2;
-        while (thread_current()->fd_table[fd_index] != NULL && fd_index < 128) {
-            fd_index++;
-        }
-        if (fd_index > 127) {
-            lock_release(&syscall_lock);
-            f->eax = -1;
-        }
-        thread_current()->fd_table[fd_index] = new_file;
         lock_release(&syscall_lock);
-        f->eax = fd_index;
     }
 
     if (args[0] == SYS_FILESIZE) {
         struct file* file_opened = thread_current()->fd_table[args[1]];
         if (file_opened == NULL) {
             f->eax = -1;
+        } else {
+            f->eax = file_length(file_opened);
         }
-        f->eax = file_length(file_opened);
     }
 
     if (args[0] == SYS_READ) {
@@ -88,62 +90,63 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
             lock_release(&syscall_lock);
             f->eax = -1;
         }
-        if (args[1] == 0) {
+        else if (args[1] == 0) {
             uint8_t* buffer = (uint8_t*) args[2];
             for (int i = 0; i < (int) args[3]; i++) {
                 uint8_t key = input_getc();
                 buffer[i] = key;
             }
-            lock_release(&syscall_lock);
             f->eax = args[3];
+        } else {
+            f->eax = file_read(file_opened, (void *) args[2], args[3]);
         }
         lock_release(&syscall_lock);
-        f->eax = file_read(file_opened, (void *) args[2], args[3]);
     }
 
     if (args[0] == SYS_WRITE) {
         lock_acquire(&syscall_lock);
         struct file* file_opened = thread_current()->fd_table[args[1]];
         if (file_opened == NULL || args[1] == 0) {
-            lock_release(&syscall_lock);
             f->eax = -1;
         }
-        if (args[1] == 1) {
+        else if (args[1] == 1) {
             char* buffer = (char *) args[2];
             putbuf(buffer, args[3]);
-            lock_release(&syscall_lock);
             f->eax = args[3];
+        } else {
+            f->eax = file_write(file_opened, (void *) args[2], args[3]);
         }
         lock_release(&syscall_lock);
-        f->eax = file_write(file_opened, (void *) args[2], args[3]);
     }
 
     if (args[0] == SYS_SEEK) {
         struct file* file_opened = thread_current()->fd_table[args[1]];
         if (file_opened == NULL) {
             f->eax = -1;
+        } else {
+            file_seek(file_opened, args[2]);
+            f->eax = 0;
         }
-        file_seek(file_opened, args[2]);
-        f->eax = 0;
     }
 
     if (args[0] == SYS_TELL) {
         struct file* file_opened = thread_current()->fd_table[args[1]];
         if (file_opened == NULL) {
             f->eax = -1;
+        } else {
+            f->eax = file_tell(file_opened);
         }
-        f->eax = file_tell(file_opened);
     }
 
     if (args[0] == SYS_CLOSE) {
         lock_acquire(&syscall_lock);
         struct file* file_opened = thread_current()->fd_table[args[1]];
         if (file_opened == NULL) {
-            lock_release(&syscall_lock);
             f->eax = -1;
+        } else {
+            file_close(file_opened);
+            thread_current()->fd_table[args[1]] = NULL;
         }
-        file_close(file_opened);
-        thread_current()->fd_table[args[1]] = NULL;
         lock_release(&syscall_lock);
     }
 }
